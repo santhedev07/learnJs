e552fa371c88d8458578ce56866994792fba70a9
santhedev07 <santhedev2007@gmail.com>
2025-06-13 14:30:16 +0700

    Upload mas

---
+++ learnJs/syntaxJs.js
@@ -0,0 +1,9 @@
+var sukuPertama = 2
+var sukuKedua = 4
+var sukuKetiga = 6
+
+var sukuKeN = 12
+var nilaiB = sukuKedua - sukuPertama
+
+var deretAritmatika = sukuPertama + (sukuKeN - 1) * nilaiB
+console.log("Deret Aritmatika: " + deretAritmatika)
