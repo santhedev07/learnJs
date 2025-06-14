96fb99e70cda465855b0553f974cd8a06dbb3840
santhedev07 <santhedev2007@gmail.com>
2025-06-14 09:48:22 +0700

    Upload ver2

--- learnJs/syntaxJs.js
+++ learnJs/syntaxJs.js
@@ -1,9 +1,39 @@
-var sukuPertama = 2
-var sukuKedua = 4
-var sukuKetiga = 6
+// Example of variable declaration and assignment in JavaScript
+// Using 'let' to declare variables
+let a, b, c;
+a = 5;
+b = 10;
+c = a + b;
+console.log(c); // Output: 15
 
-var sukuKeN = 12
-var nilaiB = sukuKedua - sukuPertama
+// Example of a variable declaration and assignment in JavaScript
+// using var keyword
+var x;
+let y;
+x = 20;
+y = 30;
 
-var deretAritmatika = sukuPertama + (sukuKeN - 1) * nilaiB
-console.log("Deret Aritmatika: " + deretAritmatika)
+let z = x + y;
+console.log(z); // Output: 50
+
+// Example of a variable declaration and assignment in JavaScript
+// using const keyword
+const pi = 3.14;
+var radius = 5;
+let area = pi * radius * radius;
+console.log(area); // Output: 78.5
+
+// opetator precedence example
+let m = 10;
+let n = 5;
+let result = (m + n) * 2; // Multiplication has higher precedence than addition
+console.log(result); // Output: 20
+// Example of using template literals for string interpolation
+
+// operator assignment
+let firstVar = 20;
+firstVar += 4
+console.log(firstVar)
+let secVar = 40;
+secVar ++
+console.log(secVar) // Output: 41
